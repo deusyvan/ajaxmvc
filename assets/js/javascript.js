43a80046363a60828e7500fbbba8f347948b829a
deusyvan <deusyvan@gmail.com>
2018-12-19 08:01:41 -0200

    Estrutura do Ajax

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,13 @@
 $(function(){
 	$('button').on('click', function(){
 		alert("Funcionou!");
+		$.ajax({
+			url: '...',
+			type: 'POST',
+			data: {nome: 'Deusyvan'},
+			success: function(r){
+				
+			}
+		});
 	});
 });
